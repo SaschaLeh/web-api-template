1e4a22d6dd13c3e487be43d2d957e1bc1eb55fe6
Sascha Lehmann <sascha.lehmann92@gmail.com>
2021-12-06 07:46:42 +0100

    Added Comment in CORS Configuration

--- WebApiTemplate/WebApiTemplate/Extensions/ServiceExtensions.cs
+++ WebApiTemplate/WebApiTemplate/Extensions/ServiceExtensions.cs
@@ -5,6 +5,7 @@
         public static void ConfigureCors(this IServiceCollection services) =>
             services.AddCors(options =>
             {
+                //Please DON'T use in PRODUCTION!! Add specific origins!
                 options.AddPolicy("CorsPolicy", builder =>
                     builder.AllowAnyOrigin()
                     .AllowAnyMethod()
